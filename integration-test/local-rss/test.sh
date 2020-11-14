6891b84b623a62d918d58853131fc48647635375
nicolas <nhomble@terpmail.umd.edu>
2020-11-14 01:47:30 -0500

    more logs

--- integration-test/local-rss/test.sh
+++ integration-test/local-rss/test.sh
@@ -2,12 +2,13 @@
 
 docker run -d feed-cli-test:0.1.0
 
-until $(curl --output /dev/null --silent --head --fail http://localhost:8000/rss.xml); do
-  printf '.'
+ENDPOINT="http://localhost:8000/rss.xml"
+until $(curl --output /dev/null --silent --head --fail $ENDPOINT); do
+  curl --head --fail $ENDPOINT
   sleep 5
 done
 
-echo "http://localhost:8000/rss.xml" | go run feed.go >out.html
+echo "$ENDPOINT" | go run feed.go >out.html
 tail -n9 out.html >diff1
 tail -n9 integration-test/local-rss/output.html >diff2
 
