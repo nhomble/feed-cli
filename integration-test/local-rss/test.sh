68884306a326a1cbbf5c82a65c51c7a63d086df2
nhomble <nhomble@terpmail.umd.edu>
2022-12-11 22:34:18 -0500

    use actions

--- integration-test/local-rss/test.sh
+++ integration-test/local-rss/test.sh
@@ -1,20 +1,6 @@
 #!/usr/bin/env sh
 
-clean() {
-  docker kill test
-  rm -f diff1
-  rm -f diff2
-  rm -f out.html
-}
-
-docker run -p 8000:8000 --name test -d feed-cli-test:0.1.0
-
 ENDPOINT="http://localhost:8000/rss.xml"
-until $(curl --output /dev/null --silent --head --fail $ENDPOINT); do
-  curl $ENDPOINT
-  docker logs test
-  sleep 5
-done
 
 echo "Done waiting for mock rss feed"
 
@@ -32,8 +18,7 @@ DIFF=$(diff diff1 diff2)
 if [ "$DIFF" != "" ]; then
   echo "THERE WAS A DIFF!"
   echo $DIFF
-  clean
   exit 1
-else
-  clean
 fi
+
+exit 0
